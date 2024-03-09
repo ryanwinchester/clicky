d832e108b7bddc2dde1dcddae5301807925c5f2d
Ryan Winchester <ryan@winchester.dev>
2024-03-09 10:19:40 -0400

    rainbow palette

--- lib/clicky_web/live/clicky_live.ex
+++ lib/clicky_web/live/clicky_live.ex
@@ -3,7 +3,7 @@ defmodule ClickyWeb.ClickyLive do
 
   require Logger
 
-  @bg_colors ~w[bg-red-500 bg-green-500 bg-yellow-500 bg-purple-500 bg-indigo-500 bg-blue-500]
+  @bg_colors ~w[bg-red-500 bg-orange-500 bg-yellow-500 bg-green-500 bg-blue-500 bg-indigo-500 bg-purple-500]
 
   @impl true
   def mount(_, _, socket) do
